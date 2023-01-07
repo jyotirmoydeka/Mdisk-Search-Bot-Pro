8395ea9636b7a1fce963dbdf3783241d1683c59f
jyotirmoydeka <68185650+jyotirmoydeka@users.noreply.github.com>
2023-01-07 16:31:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -133,6 +133,9 @@ async def message_handler(event):
             author=Config.BOT_USERNAME
         )
         message = f'**Click Here 👇 For "{event.text}"**\n\n[🍿🎬 {str(event.text).upper()}\n🍿🎬 {str("Click me for results").upper()}]({tgraph_result})'
+        
+        newbutton = [Button.url('🍿 𝐁𝐀𝐂𝐊𝐔𝐏 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 🍿',
+                                    f'https://t.me/movies_request_back?start=Watch')],
                       
         await txt.delete()
         result = await event.reply(message, link_preview=False)
